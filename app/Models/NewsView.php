8a57cf41681f947b4daa93dee88cc248c17e01ef
Aleksandr <golinov95@gmail.com>
2021-01-20 19:09:53 +0200

    remove dd

--- app/Models/NewsView.php
+++ app/Models/NewsView.php
@@ -2,9 +2,9 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\Pivot;
 
-class NewsView extends Model
+class NewsView extends Pivot
 {
     protected $table = 'users_news';
 }
